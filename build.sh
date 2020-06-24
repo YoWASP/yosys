0443d462466d01da9a0af1740b2115ece443958f
whitequark <whitequark@whitequark.org>
2020-06-24 20:34:22 +0000

    Use out-of-tree builds.

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ WASI_SDK=wasi-sdk-11.0
 WASI_SDK_URL=https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-linux.tar.gz
 if ! [ -d ${WASI_SDK} ]; then curl -L ${WASI_SDK_URL} | tar xzf -; fi
 
-cat >yosys-src/Makefile.conf <<END
+mkdir -p yosys-build
+cat >yosys-build/Makefile.conf <<END
 export PATH := $(pwd)/${WASI_SDK}/bin:${PATH}
 WASI_SYSROOT := $(pwd)/${WASI_SDK}/share/wasi-sysroot
 
@@ -19,7 +20,7 @@ ENABLE_ZLIB := 0
 CXXFLAGS += -flto
 LDFLAGS += -flto -Wl,--strip-all
 END
-make -C yosys-src PRETTY=0 CXX="ccache clang"
+make -C yosys-build -f ../yosys-src/Makefile PRETTY=0 CXX="ccache clang"
 
 rm -rf pypi/yowasp_yosys/share
-cp -r yosys-src/yosys.wasm yosys-src/share pypi/yowasp_yosys/
+cp -r yosys-build/yosys.wasm yosys-build/share pypi/yowasp_yosys/
