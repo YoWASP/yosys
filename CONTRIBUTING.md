69e9aa271d6df5ec19ab01cba21972dfe7fac94c
Catherine <whitequark@whitequark.org>
2023-02-06 12:29:09 +0000

    Migrate from setup.py to pyproject.toml.

---
+++ CONTRIBUTING.md
@@ -0,0 +1,9 @@
+YoWASP Yosys packages
+=====================
+
+Building
+--------
+
+The primary build environment for this repository is the `ubuntu-latest` GitHub CI runner; packages are built on every push and automatically published from the `release` branch to PyPI.
+
+To reduce maintenance overhead, the only development environment we will support for this repository is x86_64 Linux.
--- pypi/setup.py
+++ pypi/setup.py
@@ -1,5 +1,5 @@
 import re
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools_scm.git import parse as parse_git
 
 
@@ -20,44 +20,6 @@ def version():
     return upstream_version + package_version
 
 
-def long_description():
-    with open("../README.md") as f:
-        return f.read()
-
-
 setup(
-    name="yowasp-yosys",
     version=version(),
-    author="whitequark",
-    author_email="whitequark@whitequark.org",
-    description="Yosys Open SYnthesis Suite",
-    long_description=long_description(),
-    long_description_content_type="text/markdown",
-    license="ISC", # same as Yosys
-    python_requires="~=3.5",
-    setup_requires=["setuptools_scm", "wheel"],
-    install_requires=[
-        "importlib_resources; python_version<'3.9'",
-        "yowasp-runtime~=1.0"
-    ],
-    packages=["yowasp_yosys"],
-    package_data={"yowasp_yosys": [
-        "*.wasm",
-        "share/*", "share/**/*", "share/**/**/*", "share/**/**/**/*", # why
-    ]},
-    entry_points={
-        "console_scripts": [
-            "yowasp-yosys = yowasp_yosys:_run_yosys_argv",
-            "yowasp-yosys-smtbmc = yowasp_yosys:_run_yosys_smtbmc_argv",
-            "yowasp-sby = yowasp_yosys:_run_sby_argv",
-        ],
-    },
-    project_urls={
-        "Homepage": "https://yowasp.github.io/",
-        "Source Code": "https://github.com/YoWASP/yosys",
-        "Bug Tracker": "https://github.com/YoWASP/yosys/issues",
-    },
-    classifiers=[
-        "License :: OSI Approved :: ISC License (ISCL)",
-    ],
 )
