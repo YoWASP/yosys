4199925d14382958b92a22d9e7233620c64b80af
Catherine <whitequark@whitequark.org>
2024-01-07 02:04:09 +0000

    [autorelease] [skip py] Update `@yowasp/runtime`.
    This commit also ships a single unified (Node+browser) bundle, and
    consequently requires extension version 0.2.4 or later.

--- package-npmjs.sh
+++ package-npmjs.sh
@@ -6,10 +6,7 @@ cd $(dirname $0)/npmjs
 
 ${PYTHON} prepare.py
 npm install
-npm run transpile
-npm run pack
-npm run build:node
-npm run build:browser
+npm run all
 
 mkdir -p dist
 npm pack --pack-destination dist
