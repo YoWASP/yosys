655f5417b27a239efa0a9c82a31a26cdf5b8a4df
Catherine <whitequark@whitequark.org>
2024-02-13 08:12:59 +0000

    [skip py] Update `@yowasp/runtime`.

--- npmjs/lib/api.d.ts
+++ npmjs/lib/api.d.ts
@@ -2,10 +2,14 @@ export type Tree = {
     [name: string]: Tree | string | Uint8Array
 };
 
+export type InputStream =
+    (byteLength: number) => Uint8Array | null;
+
 export type OutputStream =
     (bytes: Uint8Array | null) => void;
 
 export type RunOptions = {
+    stdin?:  InputStream  | null;
     stdout?: OutputStream | null,
     stderr?: OutputStream | null,
     decodeASCII?: boolean
