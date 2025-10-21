a8c8b549904a5adf428ce13ff52f7c9c3470eff6
Catherine <whitequark@whitequark.org>
2025-10-21 00:26:33 +0000

    [autorelease] [skip py] Update JS runtime.

--- npmjs/lib/api.js
+++ npmjs/lib/api.js
@@ -1,9 +1,10 @@
 import { Application } from '@yowasp/runtime';
+import * as resources from '../gen/yosys-resources.js';
 import { instantiate } from '../gen/yosys.js';
 
 export { Exit } from '@yowasp/runtime';
 
-const yosys = new Application(() => import('./resources-yosys.js'), instantiate, 'yowasp-yosys');
+const yosys = new Application(resources, instantiate, 'yowasp-yosys');
 const runYosys = yosys.run.bind(yosys);
 
 export { runYosys };
